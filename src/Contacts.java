1dfcf85a0daa6e5971cc8e9278a7991da92be81d
Anwar Lopez <anwarlo247@gmail.com>
2019-04-03 15:59:14 -0500

    first draft

---
+++ src/Contacts.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.lang.String;
+
+public class Contacts {
+
+    private String name;
+
+    private String phone;
+
+
+
+
+    public Contacts(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+
+}
